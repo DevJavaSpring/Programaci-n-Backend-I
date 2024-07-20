adf3510f1d65c6fb265ec25e68a821a886cee918
DevJavaSpring <javareactpythonspringdeveloper@outlook.com>
2024-07-20 19:46:18 -0300

    Defimicion de routes y consumo de servicios de carrito y producto

--- src/app.js
+++ src/app.js
@@ -9,3 +9,15 @@ app.use(express.urlencoded({ extended: true }));
  * @returns
  */
 app.listen(8080,console.log("Servidor backend corriendo en el puerto 8080"))
+
+/**
+ * ENDPONINT DE PRODUCTOS
+ */
+import routerProductos from './routes/productos.router.js';
+app.use('/api/products', routerProductos);
+
+/**
+ * ENDPONINT DE CARRITO
+ */
+import routerCarritos from './routes/carritos.router.js';
+app.use('/api/carts', routerCarritos);
